82d17efb783a073ebae7037310ad0d68bd2756b5
Vanessa Bertoldo <126096822+Vanessa-Bertoldo@users.noreply.github.com>
2025-01-17 08:30:25 -0300

    reestructured folders

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TestController } from './test/test.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '',
+      database: 'heroes_db',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController, TestController],
   providers: [AppService],
 })
